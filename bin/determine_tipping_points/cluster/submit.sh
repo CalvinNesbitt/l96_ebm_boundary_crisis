476958989062a4f23fd5194ebe1c75c039078c43
Calvin Nesbitt <c.f.nesbitt@student.reading.ac.uk>
2023-02-23 11:21:27 +0000

    Wrote some scripts for testing on the cluster

--- bin/determine_tipping_points/cluster/submit.sh
+++ bin/determine_tipping_points/cluster/submit.sh
@@ -3,5 +3,6 @@ NOW=$(date +"%Y-%m-%d-%T")
 run_directory="$EPHEMERAL/L96-EBM-DET/Tipping-Points/Run-Dirs/$NOW"
 mkdir -p $run_directory
 cp $HOME/Thesis-Computing/Determinisitc/l96_ebm_boundary_crisis/bin/determine_tipping_points/cluster/*.sh $run_directory
+cp -r $HOME/Thesis-Computing/Determinisitc/l96_ebm_boundary_crisis/src $run_directory
 cd $run_directory
 qsub determine_tipping_points.sh
