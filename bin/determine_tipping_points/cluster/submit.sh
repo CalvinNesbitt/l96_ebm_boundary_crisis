88a5647315cea581bfd2fb788eb8aa2eea40713b
Calvin Nesbitt <c.f.nesbitt@student.reading.ac.uk>
2023-02-23 11:19:17 +0000

    Wrote some scripts for testing on the cluster

--- bin/determine_tipping_points/cluster/submit.sh
+++ bin/determine_tipping_points/cluster/submit.sh
@@ -5,7 +5,3 @@ mkdir -p $run_directory
 cp $HOME/Thesis-Computing/Determinisitc/l96_ebm_boundary_crisis/bin/determine_tipping_points/cluster/*.sh $run_directory
 cd $run_directory
 qsub determine_tipping_points.sh
-
-/Users/cfn18/Documents/PhD-Work/Thesis-Computing/Determinisitc/l96_ebm_boundary_crisis/bin/determine_tipping_points
-
-/rds/general/user/cfn18/home/
