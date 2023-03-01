685a94fb0673cbb0e75b95d738475c7aff7205fb
Calvin Nesbitt <c.f.nesbitt@student.reading.ac.uk>
2023-03-01 11:24:58 +0000

    Updated config for long run

--- cfg/cfg_writer.py
+++ cfg/cfg_writer.py
@@ -8,17 +8,22 @@ import json
 
 if __name__ == "__main__":
 
-    cfg_file = Path("cfg/TEST_CONFIG.json")
+    cfg_file = Path("cfg/longer_run.json")
 
     # W Setups
-    w_S_values = np.linspace(7.7, 7.8, 3)
+    w_S_values = np.linspace(7.7, 7.8, 10)
     w_setups = list(itertools.product(list(w_S_values), ["w"], range(0, 500)))
 
     # SB Setups
-    sb_S_values = np.linspace(15.25, 15.3, 3)
+    sb_S_values = np.linspace(15.0, 15.2, 10)
     sb_setups = list(itertools.product(list(sb_S_values), ["sb"], range(0, 500)))
 
     all_setups = w_setups + sb_setups
 
-    config = {"integration_time": 100, "dt": 0.1, "all_setups": all_setups, "results_file": "TEST.csv"}
+    config = {
+        "integration_time": 1.0e6,
+        "dt": 0.1,
+        "all_setups": all_setups,
+        "results_file": "transient_lifetimes2.csv",
+    }
     json.dump(config, cfg_file.open("w"))
