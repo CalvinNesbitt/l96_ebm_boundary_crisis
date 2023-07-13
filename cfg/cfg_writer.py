9dd5020ba9b95ed249c95a63dce5d77d5cca58af
Calvin Nesbitt <c.f.nesbitt@student.reading.ac.uk>
2023-07-13 18:36:18 +0100

    Wrote big config

--- cfg/cfg_writer.py
+++ cfg/cfg_writer.py
@@ -10,11 +10,11 @@ if __name__ == "__main__":
     cfg_file = Path("cfg/transient_lifetime_130723_BIG.json")
 
     # SB Setups
-    sb_S_values = np.linspace(15.35, 15.45, 50)
+    sb_S_values = np.linspace(15.35, 15.45, 10)
     sb_setups = list(itertools.product(list(sb_S_values), ["sb"], range(0, 500)))
 
     # W Setups
-    w_S_values = np.linspace(7.7, 7.8, 50)
+    w_S_values = np.linspace(7.7, 7.8, 10)
     w_setups = list(itertools.product(list(w_S_values), ["w"], range(0, 500)))
 
     all_setups = sb_setups + w_setups
