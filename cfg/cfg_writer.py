147dfc7c35c53e9a60e4eba7aef5d2eeec44d246
Calvin Nesbitt <c.f.nesbitt@student.reading.ac.uk>
2023-07-13 17:04:18 +0100

    Updated config

--- cfg/cfg_writer.py
+++ cfg/cfg_writer.py
@@ -7,14 +7,14 @@ import json
 
 
 if __name__ == "__main__":
-    cfg_file = Path("cfg/transient_lifetime_130723.json")
+    cfg_file = Path("cfg/transient_lifetime_130723_2.json")
 
     # SB Setups
-    sb_S_values = np.linspace(15.5694, 15.1093, 10)
+    sb_S_values = np.linspace(15.15, 15.3, 50)
     sb_setups = list(itertools.product(list(sb_S_values), ["sb"], range(0, 100)))
 
     # W Setups
-    w_S_values = np.linspace(7.2205, 7.6868, 10)
+    w_S_values = np.linspace(7.65, 7.8, 50)
     w_setups = list(itertools.product(list(w_S_values), ["w"], range(0, 100)))
 
     all_setups = sb_setups + w_setups
@@ -23,6 +23,6 @@ if __name__ == "__main__":
         "integration_time": 1.0e5,
         "dt": 0.25,
         "all_setups": all_setups,
-        "results_file": "/rds/general/user/cfn18/home/Thesis-Computing/Determinisitc/l96_ebm_boundary_crisis/data/transient_liftime_130723.csv",
+        "results_file": "/rds/general/user/cfn18/home/Thesis-Computing/Determinisitc/l96_ebm_boundary_crisis/data/transient_lifetime_130723_2.csv",
     }
     json.dump(config, cfg_file.open(mode="w"))
